319a281da3b415410b23d583415b017d88befe23
buonviyeuanh456@gmail.com <buonviyeuanh456@gmail.com>
2024-12-27 20:56:34 +0700

    upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Bae à!',
+    introDesc: `Trái đất vốn lạ thường.
+    Mà sao em cứ đi nhầm đường.
+    Lạc vào tim anh lẻ loi.
+    Đằng sau chữ yêu đây là thương.`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Làm ngừi iu anh nhé Bae ',
+    btnYes: 'Okiii <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Gặp được em là sự may mắn, được yêu em nữa thì ông trời đã quá ưu ái anh rồi <3.',
+    btnAccept: 'Moahhhhhh <3',
+    messLink: 'http://fb.com/xuangiau914928' 
+}
